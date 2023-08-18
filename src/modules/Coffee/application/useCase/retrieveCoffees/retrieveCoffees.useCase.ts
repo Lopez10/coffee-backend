efa664d37e2a590a5924bf5fdc3e0161b1abd19b
Lopez10 <ljuan7397@gmail.com>
2023-08-18 09:22:59 +0200

    Modify to retrieve coffees

--- src/modules/Coffee/application/useCase/retrieveCoffees/retrieveCoffees.useCase.ts
+++ src/modules/Coffee/application/useCase/retrieveCoffees/retrieveCoffees.useCase.ts
@@ -2,23 +2,25 @@ import { UseCase } from '@common';
 import { Coffee } from 'src/modules/Coffee/domain/Coffee.entity';
 import { CoffeeRepositoryPort } from 'src/modules/Coffee/domain/coffee.repository.port';
 
-export interface RetrieveCoffeeDTO {
+export interface RetrieveCoffeesDTO {
   name?: string;
   origin?: string;
   height?: number;
   roast?: string;
+  userId?: string;
 }
 export class RetrieveCoffeeUseCase
-  implements UseCase<RetrieveCoffeeDTO, Promise<Coffee[]>>
+  implements UseCase<RetrieveCoffeesDTO, Promise<Coffee[]>>
 {
   constructor(private readonly coffeeRepository: CoffeeRepositoryPort) {}
 
-  async run(request?: RetrieveCoffeeDTO): Promise<Coffee[]> {
+  async run(request?: RetrieveCoffeesDTO): Promise<Coffee[]> {
     const coffeeValues = {
       name: request?.name,
       origin: request?.origin,
       height: request?.height,
       roast: request?.roast,
+      userId: request?.userId,
     };
 
     const coffee = await this.coffeeRepository.findByCriteria(coffeeValues);
